41c7de09cfa39d39eacb55cf988e05f1ed471ab8
BrunoBarroso09 <brunomcbarroso@outlook.pt>
2025-12-20 19:59:36 +0000

    04 - Assignment Operators

---
+++ 04_Assignment_operators.py
@@ -0,0 +1,21 @@
+#Assignment Operators
+
+x = 2
+
+x += 3 # x = x + 3
+print('x = x + 2:', x)
+
+x -= 2
+print('x = x - 2:', x)
+
+x *= 2
+print('x = x * 2:', x)
+
+x /= 2
+print('x = x / 2:', x)
+
+x %= 2
+print('x = x % 2:', x)
+
+x **= 2
+print('x = x ** 2:', x)

ff57552104eb6eeb66e9054e1a7256bb71255576
BrunoBarroso09 <brunomcbarroso@outlook.pt>
2025-12-20 19:56:06 +0000

    01 - print function

---
+++ 01_print.py
@@ -0,0 +1,12 @@
+#Print function
+
+print('Hello World!')
+
+#Using double quotes inside simples quotes
+print("I'm a 'string'")
+
+#Using apostrophe with escape
+print('I\'m writing in English')
+
+#Concatenate strings
+print('Marshall', 'speakers')

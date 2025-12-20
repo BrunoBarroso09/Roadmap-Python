3e0ecf5db485a15efd1daff71ea8de0c726b0c61
BrunoBarroso09 <brunomcbarroso@outlook.pt>
2025-12-20 20:00:20 +0000

    05 - More about strings

---
+++ 05_More_about_strings.py
@@ -0,0 +1,21 @@
+#More about strings
+
+#New line \n
+string = 'I\'m a\nstring'
+print(string)
+
+#Indentation \t
+string = 'I\'m a\n\tstring'
+print(string)
+
+#len - string length
+string = 'I\'m a string'
+print(len(string))
+
+#upper - All characters in upper case
+string = 'I\'m a string'
+print(string.upper())
+
+#lower - All characters in lower case
+string = 'I\'m a string'
+print(string.lower())

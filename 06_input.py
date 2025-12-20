6b26faf779ca6a4414df89254ded3bd899e32519
BrunoBarroso09 <brunomcbarroso@outlook.pt>
2025-12-20 21:05:30 +0000

    06 - Input function

---
+++ 06_input.py
@@ -0,0 +1,9 @@
+#Input function
+
+#Input function - extract information given by the user
+first_name = input('Insert first name: ')
+last_name = input('Insert last name: ')
+age = input('Insert age: ')
+print('First name:', first_name)
+print('Last name:', last_name)
+print('Age:', age)

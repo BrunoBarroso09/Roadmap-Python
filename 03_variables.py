0e229242cf2d4404b243c9caee53261cecd44ee1
BrunoBarroso09 <brunomcbarroso@outlook.pt>
2025-12-20 19:59:03 +0000

    03 - Variables

---
+++ 03_variables.py
@@ -0,0 +1,11 @@
+#Variables
+
+integer = 10 #integer
+decimal = 10.5 #decimal
+string = 'I\'m a string' #string
+boolean = True #boolean
+
+print('integer:',integer)
+print('decimal:', decimal)
+print('string:', string)
+print('boolean:', boolean)

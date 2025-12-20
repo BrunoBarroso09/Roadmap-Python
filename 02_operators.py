359b5d6088a39617e5c8f93f13ea216d9d37ebc8
BrunoBarroso09 <brunomcbarroso@outlook.pt>
2025-12-20 19:57:13 +0000

    02 - Arithmetic operators

---
+++ 02_operators.py
@@ -0,0 +1,19 @@
+# Arithmetic operators
+
+# Addiction
+print('Addiction:', 10 + 10)
+
+#Subtraction
+print('subtraction:', 10.5 - 5.5 - 4.5)
+
+#Multiplication
+print('Multiplication:', -10 * 2)
+
+#Divison
+print('Divison:', 10 / 10)
+
+#Rest
+print('Rest:', 8 % 2)
+
+#Exponent
+print("Exponent", 2 ** 5) # 2 * 2 * 2 * 2 * 2

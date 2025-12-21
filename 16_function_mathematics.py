bbfc7125558fb8b9f1e6cc003e650a7fb1a0561d
BrunoBarroso09 <brunomcbarroso@outlook.pt>
2025-12-21 14:39:06 +0000

    16 - Mathematics function

---
+++ 16_function_mathematics.py
@@ -0,0 +1,27 @@
+from math import *
+
+num = [14, 10, 22, 67, 90]
+
+#sin
+print('Result sin(): ', sin(10.5))
+#cos
+print('Result cos(): ', cos(10.5))
+#tan
+print('Result tan(): ', tan(10.5))
+
+#floor
+print('Result floor(): ', floor(10.5))
+#ceil
+print('Result ceil(): ', ceil(10.5))
+#abs
+print('Result abs(): ', abs(-10.5))
+
+#fsum
+print('Result fsum(): ', fsum(num))
+#pow
+print('Result pow(): ', pow(2, 5))
+
+#sqrt
+print('function sqrt', sqrt(10))
+#pi
+print('pi: ', pi)

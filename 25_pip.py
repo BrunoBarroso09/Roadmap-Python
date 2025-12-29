ef101b616f489b09e77e96076308e793c1bc48d1
BrunoBarroso09 <brunomcbarroso@outlook.pt>
2025-12-29 18:32:45 +0000

    25 - Install and using pip package (Camel Case)

---
+++ 25_pip.py
@@ -0,0 +1,12 @@
+#Install and using pip
+
+from camelcase import CamelCase
+
+c = CamelCase()
+
+print(c.hump("this is my string in camel case"))
+
+
+#note
+#upgrade package -> pip install -U camelcase
+#remove package -> pip uninstall camelcase

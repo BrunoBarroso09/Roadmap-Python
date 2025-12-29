d0a9334417a47083d1e2c8f08122a56dd9a318e5
BrunoBarroso09 <brunomcbarroso@outlook.pt>
2025-12-29 11:26:58 +0000

    Math module

---
+++ Module/20_module..py
@@ -0,0 +1,12 @@
+#Module
+
+from Mathematic import *
+
+value1 = 10
+value2 = 5
+
+print(add(value1, value2))
+print(subtract(value1, value2))
+print(multiply(value1, value2))
+print(divide(value1, value2))
+print(dictionary['age'])
---
+++ Module/Mathematic.py
@@ -0,0 +1,17 @@
+dictionary = {
+    'name' : 'Peter',
+    'age' : 22,
+    'last_name': 'Grid'
+}
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y
